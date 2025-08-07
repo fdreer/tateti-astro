43414a7889b81246f571c4ea6e8f3141bb88a8a7
fdreer <francodreer@gmail.com>
2025-08-07 20:55:12 -0300

    .

--- src/pages/api/online/[id].ts
+++ src/pages/api/online/[id].ts
@@ -1,16 +1,16 @@
+import supabase from '@/lib/supabase'
 import type { APIRoute } from 'astro'
-
-const roomsId = ['primera-sala', 'segunda-sala']
 
 // METODO PARA UNIRSE A UNA SALA QUE YA EXISTE
 export const POST: APIRoute = ({ params }) => {
 	const roomdId = params.id as string
+	const room = supabase.channel(roomdId)
 
-	if (!roomsId.includes(roomdId)) {
-		return new Response(JSON.stringify({ msg: 'La sala no existe' }), {
-			status: 404
-		})
-	}
+	// if (!roomsId.includes(roomdId)) {
+	// 	return new Response(JSON.stringify({ msg: 'La sala no existe' }), {
+	// 		status: 404
+	// 	})
+	// }
 
 	return new Response(JSON.stringify({ msg: 'La sala existe' }), {
 		status: 200
--- src/pages/api/online/index.ts
+++ src/pages/api/online/index.ts
@@ -1,10 +1,19 @@
+import supabase from '@/lib/supabase'
 import type { APIRoute } from 'astro'
 
 // METODO QUE CREA SALAS
 export const POST: APIRoute = async () => {
-	console.log('Metodo GET funcionando')
+	const room_id = crypto.randomUUID().split('-')[0]
 
-	const roomId = crypto.randomUUID().split('-')[0]
+	// const { data, error, status } = await supabase
+	// 	.from('rooms')
+	// 	.insert({ room_id, current_turn: 'o', board: ['x', 'x'], players: 1 })
 
-	return new Response(JSON.stringify({ roomId: roomId }), { status: 201 })
+	// console.log(data, error, status)
+
+	const newChannel = supabase.channel(room_id)
+
+	// console.log(newChannel)
+
+	return new Response(JSON.stringify({ room_id }), { status: 201 })
 }
