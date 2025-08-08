2afefb20c5831adde1006b801f1b42a88e026793
fdreer <francodreer@gmail.com>
2025-08-08 12:41:54 -0300

    .

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,15 +8,7 @@ import {
 	type User
 } from 'firebase/auth'
 
-const firebaseConfig = {
-	apiKey: 'AIzaSyDG7lch0XvGPgTV9crHOc2Bj3LkPYTXd5U',
-	authDomain: 'tateti-a6af0.firebaseapp.com',
-	databaseURL: 'https://tateti-a6af0-default-rtdb.firebaseio.com',
-	projectId: 'tateti-a6af0',
-	storageBucket: 'tateti-a6af0.firebasestorage.app',
-	messagingSenderId: '942395447408',
-	appId: '1:942395447408:web:16c0055233e9fb6684bcb5'
-}
+const firebaseConfig = {}
 
 const app = initializeApp(firebaseConfig)
 export const database = getDatabase(app)
